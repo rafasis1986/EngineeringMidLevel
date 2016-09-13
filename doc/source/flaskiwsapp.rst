3cb0b0ddb9d2c47e084740b15d9c3842941a2924
Rafael Torres <rdtr.sis@gmail.com>
2016-09-13 17:30:49 -0400

    Adding files and folders from template

---
+++ doc/source/flaskiwsapp.rst
@@ -0,0 +1,63 @@
+flaskiwsapp package
+=================================
+
+Subpackages
+-----------
+
+.. toctree::
+
+    flaskiwsapp.admin
+    flaskiwsapp.main
+    flaskiwsapp.users
+
+Submodules
+----------
+
+flaskiwsapp.app module
+------------------------------------
+
+.. automodule:: flaskiwsapp.app
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+flaskiwsapp.database module
+-----------------------------------------
+
+.. automodule:: flaskiwsapp.database
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+flaskiwsapp.extensions module
+-------------------------------------------
+
+.. automodule:: flaskiwsapp.extensions
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+flaskiwsapp.settings module
+-----------------------------------------
+
+.. automodule:: flaskiwsapp.settings
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+flaskiwsapp.utils module
+--------------------------------------
+
+.. automodule:: flaskiwsapp.utils
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Module contents
+---------------
+
+.. automodule:: flaskiwsapp
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ manage.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Management script."""
+import os
+
+from flask_script import Manager, Server, Shell
+from flask_migrate import Migrate, MigrateCommand
+from flask_script.commands import Clean, ShowUrls
+
+from flaskiwsapp.app import create_app
+from flaskiwsapp.database import db
+from flaskiwsapp.settings import DevConfig, ProdConfig
+from flaskiwsapp.users.models import User
+
+
+CONFIG = ProdConfig if os.environ.get('ONEINOTE_ENV') == 'prod' else DevConfig
+HERE = os.path.abspath(os.path.dirname(__file__))
+TEST_PATH = os.path.join(HERE, 'tests')
+
+app = create_app(CONFIG)
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def _make_context():
+    """Return context dict for a shell session so you can access app, db, and
+    the User model by default."""
+    return {'app': app, 'db': db, 'User': User}
+
+
+@manager.command
+def test():
+    """run the tests."""
+    import pytest
+    exit_code = pytest.main([TEST_PATH, '--verbose'])
+    return exit_code
+
+
+@manager.command
+def create_admin():
+    """Create a default admin user to get access to the admin panel."""
+    if (db.session.query(User) .filter(User.username == 'admin')
+            .first()) is None:
+        admin = User.create(username='admin', email="admin@example.com",
+                            is_admin=True, active=True)
+        admin.set_password('admin')
+        admin.save()
+    else:
+        print('Admin user already exists. Try to login with: \n',
+              'username: admin \n',
+              'password: admin')
+  
+
+manager.add_command('server', Server())
+manager.add_command('shell', Shell(make_context=_make_context))
+manager.add_command('db', MigrateCommand)
+manager.add_command('urls', ShowUrls())
+manager.add_command('clean', Clean())
+
+if __name__ == '__main__':
+    manager.run()
