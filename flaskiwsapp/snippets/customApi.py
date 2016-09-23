4069a7017d0bbda2aa4d436741619304df3f654f
rtorres <rtorres@gmtprime.com>
2016-09-23 14:16:31 -0430

    Customize handle error to JsonApi standard

--- flaskiwsapp/snippets/customApi.py
+++ flaskiwsapp/snippets/customApi.py
@@ -7,27 +7,29 @@ from flask_restful import Api
 from flask_jwt import JWTError
 from flask import jsonify
 import collections
+from flask_api.status import HTTP_501_NOT_IMPLEMENTED
 
+DUMMY_ERROR_CODE = '1000'
 
 
 class CustomApi(Api):
     """A simple class to keep the default Errors behaviour."""
 
     def handle_error(self, e):
+        response_dict = {'data': {}}
+        error = {}
         if isinstance(e, JWTError):
-            return jsonify(
-                collections.OrderedDict([
-                    ('status_code', e.status_code),
-                    ('error', e.error),
-                    ('description', e.description),
-                ])
-            ), e.status_code, e.headers
+            error.update({'status': e.status_code})
+            error.update({'title': e.error})
+            error.update({'detail': e.description})
+            error.update({'code': DUMMY_ERROR_CODE})
+            response_dict['data'] = error
+            return jsonify(response_dict), e.status_code, e.headers
         elif isinstance(e, Exception):
-            return jsonify(
-                collections.OrderedDict([
-                    ('status_code', HTTP_500),
-                    ('error', str(type(e))),
-                    ('description', e.args[0]),
-                ])
-            ), HTTP_500_INTERNAL_SERVER_ERROR
+            error.update({'status': HTTP_501_NOT_IMPLEMENTED})
+            error.update({'title': str(type(e))})
+            error.update({'detail': e.args[0]})
+            error.update({'code': DUMMY_ERROR_CODE})
+            response_dict['data'] = error
+            return jsonify(response_dict), HTTP_501_NOT_IMPLEMENTED, None
         return super(CustomApi, self).handle_error(e)
