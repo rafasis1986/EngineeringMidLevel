b2bdae9985f7027222ad67d46043d166f4022305
rtorres <rtorres@gmtprime.com>
2016-09-23 13:51:55 -0430

    Refactory to move the ADminForm

--- flaskiwsapp/users/forms.py
+++ flaskiwsapp/users/forms.py
@@ -3,13 +3,13 @@
 from flask_wtf import Form
 from wtforms import PasswordField, StringField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
-from flaskiwsapp.users.validators import validate_login, is_admin
+from flaskiwsapp.users.validators import validate_login
 
 
 class LoginForm(Form):
     """Basic login form."""
     email = StringField('Email', validators=[DataRequired(), Email(),
-                                             Length(min=6, max=40)])
+                                             Length(min=6, max=80)])
     password = PasswordField('Password', validators=[DataRequired(),
                                                      Length(min=6, max=50),
                                                      validate_login])
@@ -21,13 +21,3 @@ class RegisterForm(LoginForm):
                             validators=[DataRequired(),
                                         EqualTo('password',
                                         message='Passwords must match')])
-
-
-class AdminLoginForm(LoginForm):
-    """
-    Admin login form. Only users with the 'is_admin' flag pass the form
-    validation.
-    """
-    password = PasswordField('Password', validators=[DataRequired(),
-                                                     validate_login,
-                                                     is_admin])
