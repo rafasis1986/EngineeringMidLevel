98fc3214afc92a2aef78d1ff224783c804cd33b7
Rafael Torres <rdtr.sis@gmail.com>
2016-09-24 19:27:16 -0400

    Remove legacy knockout template

--- knockoutapp/src/constants/enviroment.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by rtorres on 9/15/16.
- */
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    exports.API_TOKEN = '';
-    exports.API_URL = ' http://localhost:5000/api/1.0/';
-});
-//# sourceMappingURL=enviroment.js.map
--- knockoutapp/src/constants/enviroment.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by rtorres on 9/15/16.
- */
-
-
-export const API_TOKEN = '';
-export const API_URL = ' http://localhost:5000/api/1.0/';
