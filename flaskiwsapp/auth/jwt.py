80d710269ff1d6421f4a29b9a0d424868cb5ec54
Rafael Torres <rdtr.sis@gmail.com>
2016-09-24 06:22:58 -0430

    Raise JWTErrror into authentication handler

--- flaskiwsapp/auth/jwt.py
+++ flaskiwsapp/auth/jwt.py
@@ -2,12 +2,17 @@ from flaskiwsapp.users.controllers import get_user_by_id, get_user_by_email
 from flaskiwsapp.snippets.customApi import DUMMY_ERROR_CODE
 from flask_jwt import JWTError
 from flask import jsonify
+from flaskiwsapp.snippets.exceptions.userExceptions import UserDoesnotExistsException
+from flask_api.status import HTTP_500_INTERNAL_SERVER_ERROR
 
 
 def authenticate(email, password):
-    user = get_user_by_email(email)
-    if user and user.check_password(password) and user.is_active:
-        return user
+    try:
+        user = get_user_by_email(email)
+        if user and user.check_password(password) and user.is_active:
+            return user
+    except UserDoesnotExistsException as e:
+        raise JWTError(error=str(type(e)), description=e.message)
 
 
 def identity(payload):
@@ -22,6 +27,13 @@ def error_handler(e):
         error.update({'status': e.status_code})
         error.update({'title': e.error})
         error.update({'detail': 'Auth Failed: {}'.format(e.description)})
+        error.update({'code': DUMMY_ERROR_CODE})
+        response_dict['data'] = error
+        return jsonify(response_dict), e.status_code, e.headers
+    elif isinstance(e, Exception):
+        error.update({'status': HTTP_500_INTERNAL_SERVER_ERROR})
+        error.update({'title': str(type(e))})
+        error.update({'detail': 'Auth Failed: {}'.format(e.args[0])})
         error.update({'code': DUMMY_ERROR_CODE})
         response_dict['data'] = error
         return jsonify(response_dict), e.status_code, e.headers
