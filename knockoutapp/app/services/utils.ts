3f280d297b548c6108602d1f4069808c8aafe9fa
Rafael Torres <rdtr.sis@gmail.com>
2016-09-28 09:01:52 -0400

    Use js-cookies to get cookie

--- knockoutapp/app/services/utils.ts
+++ knockoutapp/app/services/utils.ts
@@ -1,19 +1,8 @@
 /**
  * Created by rtorres on 9/25/16.
  */
-
+import * as Cookies from 'js-cookie';
 
 export function getCookie(cname: string): string {
-    let name: string = cname + '=';
-    let ca: string[] = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        let c: any = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
-        }
-    }
-    return null;
+    return Cookies.get(cname);
 }
