03faf5324fd5d938d41f0c290025acd5d8a278d2
Rafael Torres <rdtr.sis@gmail.com>
2016-09-28 06:03:11 -0430

    OAuth response send enviroment

--- flaskiwsapp/auth/views.py
+++ flaskiwsapp/auth/views.py
@@ -62,5 +62,6 @@ def call_back():
     response = redirect(response_url, code=302)
     expire_date = datetime.datetime.now() + current_app.config['JWT_EXPIRATION_DELTA']
     response.set_cookie('Authorization', value=token, expires=expire_date)
+    response.set_cookie('Env', value=current_app.config['ENV'], expires=expire_date)
     response.set_cookie('urls', value=get_api_urls(current_app), expires=expire_date)
     return response
