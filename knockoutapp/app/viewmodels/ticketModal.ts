73d3519e749a78688f829a2e6917c2f05b46d011
Rafael Torres <rdtr.sis@gmail.com>
2016-10-01 17:25:24 -0400

    Create Ticket Model

---
+++ knockoutapp/app/viewmodels/ticketModal.ts
@@ -0,0 +1,7 @@
+import * as dialog from 'plugins/dialog';
+import CustomModal = require("./customModal");
+
+class TicketModal extends CustomModal{
+}
+
+export = CustomModal
--- knockoutapp/tasks/paths.js
+++ knockoutapp/tasks/paths.js
@@ -8,7 +8,7 @@ module.exports = {
     output:outputRoot,
     source_js: appRoot + '**/*.js',
     source_ts: appRoot + '**/*.ts',
-    source_dts:['typings/globals/**/*.d.ts','jspm_packages/**/*.d.ts'],
+    source_dts:['typings/globals/**/*.d.ts','typings_local/*.d.ts','jspm_packages/**/*.d.ts'],
     html: appRoot + '**/*.html',
     css: contentRoot + '**/*.css'
 }
