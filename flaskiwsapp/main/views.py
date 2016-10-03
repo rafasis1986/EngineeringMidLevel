cb7cffed536dc5dba2e06d7f6597e37240efe7c3
rtorres <rtorres@gmtprime.com>
2016-10-03 17:37:24 -0430

    redirect to web app

--- flaskiwsapp/main/views.py
+++ flaskiwsapp/main/views.py
@@ -7,6 +7,8 @@ from flask import Blueprint
 
 from flaskiwsapp.extensions import login_manager
 from flaskiwsapp.users.controllers.userControllers import get_user_by_id
+from werkzeug.utils import redirect
+from flask.globals import current_app
 
 
 main_blueprint = Blueprint('main', __name__, url_prefix='',
@@ -16,3 +18,9 @@ main_blueprint = Blueprint('main', __name__, url_prefix='',
 @login_manager.user_loader
 def load_user(user_id):
     return get_user_by_id(int(user_id))
+
+
+@main_blueprint.route('/')
+def index():
+    """Renders the main page page."""
+    return redirect(current_app.config['APP_URL'], code=302)
